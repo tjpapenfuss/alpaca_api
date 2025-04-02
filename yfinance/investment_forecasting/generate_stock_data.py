0b7478c223182c6b71f93fdda8355d9bdfcab3a2
Tanner Papenfuss <tjpapenfuss@gmail.com>
2025-04-02 16:31:45 -0700

    Updating generate stock data.

--- yfinance/investment_forecasting/generate_stock_data.py
+++ yfinance/investment_forecasting/generate_stock_data.py
@@ -2,30 +2,30 @@ import yfinance as yf
 import json
 from utils.data_loader import extract_top_tickers_from_csv
 
-# spy_data = yf.download(
-#                 tickers='SPY',
-#                 start='2020-01-01',
-#                 end='2022-01-01',
-#                 interval="1d",
-#                 group_by='ticker'
-#             )
+spy_data = yf.download(
+                tickers='SPY',
+                start='2018-01-01',
+                end='2025-04-01',
+                interval="1d",
+                group_by='ticker'
+            )
 
-# spy_data.to_pickle('spy-20-22.pkl')
+spy_data.to_pickle('spy-18-25.pkl')
 
 tickers_source='/Users/tannerpapenfuss/finance_testing/alpaca_api/yfinance/investment_forecasting/sp500_companies.csv'
 
 # Get tickers from configuration
 tickers = extract_top_tickers_from_csv(
     csv_file=tickers_source, 
-    top_n=50
+    top_n=500
 )
 
 ticker_50 = yf.download(
                 tickers=tickers,
-                start='2020-01-01',
-                end='2022-01-01',
+                start='2018-01-01',
+                end='2025-04-01',
                 interval="1d",
                 group_by='ticker'
             )
 
-ticker_50.to_pickle('top-50-20-22.pkl')
+ticker_50.to_pickle('top-500-18-25.pkl')
